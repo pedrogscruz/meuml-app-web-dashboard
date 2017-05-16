7b655c03821c12919032028335d4c2d4465ee005
clrech <clrech@gmail.com>
2017-05-16 13:47:02 -0300

    Exibe a lista de tags no sidenav.

--- src/app/common/protected/protectedControllers.js
+++ src/app/common/protected/protectedControllers.js
@@ -1,12 +1,25 @@
 angular.module('gorillascode.protected')
 
-.controller('ProtectedController', ['$state', '$mdSidenav', 'AuthenticationService',
-  'LocalUserService', 'user',
+.controller('ProtectedController', ['$log', '$state', '$mdSidenav', 'AuthenticationService',
+  'LocalUserService', 'SellerImageTagSearchService', 'user',
 
-  function($state, $mdSidenav, AuthenticationService, LocalUserService, user) {
+  function($log, $state, $mdSidenav, AuthenticationService, LocalUserService,
+           SellerImageTagSearchService, user) {
     var self = this;
 
     self.user = user;
+    self.imageTags = {};
+
+    function searchImageTags() {
+      $log.debug('Listando todas as tags');
+
+      SellerImageTagSearchService.search().then(function(response) {
+        $log.debug('Tags listadas');
+        self.imageTags = response;
+      }, function(error) {
+        NotificationService.error('Não foi possível listar as tags', error);
+      });
+    }
 
     self.toggleMenu = function() {
       $mdSidenav('sidenav-left').toggle();
@@ -17,6 +30,8 @@ angular.module('gorillascode.protected')
         $state.go('public.login');
       });
     };
+
+    searchImageTags();
   }
 ])
 
