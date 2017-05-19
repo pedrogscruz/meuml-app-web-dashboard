7c43bb080a41ba1c242efe67191d12e5467d617d
clrech <clrech@gmail.com>
2017-05-19 19:29:32 -0300

    Edição de templates.

--- src/app/custom/protected/protected.js
+++ src/app/custom/protected/protected.js
@@ -2,6 +2,7 @@ angular.module('meuml.protected', [
   'meuml.protected.home',
   'meuml.protected.image',
   'meuml.protected.image-tag',
+  'meuml.protected.template',
 ])
 
 ;
---
+++ src/app/custom/services/templateTagService.js
@@ -0,0 +1,66 @@
+angular.module('meuml.services.template-tag', [
+  'meuml.resources.template-tag'
+])
+
+.factory('SellerTemplateTagService', ['SellerTemplateTag',
+  function(SellerTemplateTag) {
+    var service = {
+      save: function(templateTag) {
+        if (templateTag.id) {
+          return SellerTemplateTag.patch(templateTag).$promise;
+        } else {
+          return SellerTemplateTag.save(templateTag).$promise;
+        }
+      },
+      delete: function(id) {
+        return SellerTemplateTag.delete({ id: id }).$promise;
+      },
+    };
+
+    return service;
+  }
+])
+
+.factory('SellerTemplateTagSearchService', ['$q', 'SellerTemplateTagSearch',
+  function($q, SellerTemplateTagSearch) {
+    // Cache com as tags os templates
+    var tags = {};
+
+    var service = {
+      /**
+       * Faz a pesquisa das 1.0000 tags mais usadas nos templates e armazena o resultado em cache.
+       */
+      search: function() {
+        // Se a pesquisa já foi feita retorna o resultado em cache.
+        if (tags.result) {
+          return $q.resolve(tags);
+        }
+
+        var parameters = {
+          results_per_page: 1000,
+        };
+
+        return SellerTemplateTagSearch.query(parameters, function(response) {
+          tags = response;
+        }).$promise;
+      },
+      addTags: function(newTags) {
+        angular.forEach(newTags, function(newTag) {
+          var tagExists = tags.result.some(function(tag) {
+            return tag == newTag;
+          });
+
+          if (!tagExists) {
+            tags.result.push(newTag);
+          }
+        });
+
+        tags.result.sort();
+      },
+    };
+
+    return service;
+  }
+])
+
+;
