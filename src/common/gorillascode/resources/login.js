3ffc16209b844b4e1accd3ee4b6d80bd62509678
Cleber <clrech@gmail.com>
2017-05-30 01:42:54 -0300

    Pode fazer login usando a senha importada do Django.

--- src/common/gorillascode/resources/login.js
+++ src/common/gorillascode/resources/login.js
@@ -4,7 +4,7 @@ angular.module('gorillascode.resources.login', [
 
 .factory('Login', ['configuration', 'ResourceFactory',
   function (configuration, ResourceFactory) {
-    return new ResourceFactory(configuration.apiUrl, 'login');
+    return new ResourceFactory(configuration.apiUrl, 'user/_login');
   }]
 )
 
