74ad9c612e4c3abc8be151dc79a962b6df4b5dd1
clrech <clrech@gmail.com>
2017-09-11 18:18:46 -0300

    Usando OAuthService para pegar o token.

--- src/app/custom/protected/migration-request/migrationRequestControllers.js
+++ src/app/custom/protected/migration-request/migrationRequestControllers.js
@@ -1,19 +1,20 @@
 angular.module('meuml.protected.migration-request')
 
 .controller('MigrationRequestController', ['$window', 'NotificationService', 'MigrationService',
-  'configuration', 'migrations',
+  'OAuthService', 'configuration', 'migrations',
   
-  function($window, NotificationService, MigrationService, configuration, migrations) {
+  function($window, NotificationService, MigrationService, OAuthService, configuration,
+           migrations) {
+
     var self = this;
 
     self.migrations = migrations;
 
     self.start = function(migration) {
-      // Função que será chamada quando a pop-up aberta for fechada
-      $window.oauthCallback = function(accessToken, refreshToken) {
+      OAuthService.getToken(function(response) {
         var parameters = {
-          access_token: accessToken,
-          refresh_token: refreshToken,
+          access_token: response.accessToken,
+          refresh_token: response.refreshToken,
           migration_id: migration.id,
         };
 
@@ -23,17 +24,7 @@ angular.module('meuml.protected.migration-request')
           NotificationService.error('Não foi possível começar a correção. Tente novamente mais ' +
               'tarde.', error);
         });
-      };
-      
-      var authorizeUrl = configuration.apiUrl + '/_oauth/authorize';
-      var height = 510;
-      var width = 830;
-      var left = parseInt((screen.availWidth - width) / 2);
-      var top = parseInt((screen.availHeight - height) / 2);
-
-      window.open(authorizeUrl, '', 'toolbar=no,status=no,location=yes,menubar=no,resizable=no,' +
-          'scrollbars=no,width=' + width + ',height=' + height + ',left=' + left+ ',top=' + top +
-          'screenX=' + left+ ',screenY=' + top);
+      });
     };
   }
 ])
